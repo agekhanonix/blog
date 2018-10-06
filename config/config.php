66913c4694795e36e7beaedfe272307826a2ba42
Thierry CHARPENTIER <38577010+agekhanonix@users.noreply.github.com>
2018-10-06 13:07:57 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@
     /*define('DBHOST', '127.0.0.1');*/
     define('DBPORT', '3306');
     define('DBUSER', 'agekhanokcroot');
-    define('DBPASS', 'CZpvNAP3');
+    define('DBPASS', '******');
     define('DBNAME', 'agekhanokcroot');
 
     /* ===             WEB SITE PARAMETERS            === */
